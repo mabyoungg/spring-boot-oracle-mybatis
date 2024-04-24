bbb5b6a905407311c75d068460fa92edf35ef393
mabyoungg <mabyoungg@gmail.com>
2024-04-24 20:16:02 +0900

    init: 프로젝트 생성

---
+++ src/main/java/org/example/springbootoraclemybatis/SpringBootOracleMybatisApplication.java
@@ -0,0 +1,13 @@
+package org.example.springbootoraclemybatis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootOracleMybatisApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootOracleMybatisApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/springbootoraclemybatis/SpringBootOracleMybatisApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.springbootoraclemybatis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootOracleMybatisApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
